c0e24920f48e767d79e9b178fb180e291af1bb80
mw <magicmaow@126.com>
2022-05-25 09:37:05 +0800

    feat: 一些算法

---
+++ 排序/index.js
@@ -0,0 +1,165 @@
+// 基础排序算法：
+// 冒泡排序
+// 插入排序
+// 选择排序
+// 进阶排序算法
+// 归并排序
+// 快速排序
+
+// 冒泡排序的过程，就是从第一个元素开始，重复比较相邻的两个项，若第一项比第二项更大，则交换两者的位置；反之不动。
+function bubbleSort(arr) {
+  // 缓存数组长度
+  const len = arr.length;
+  // 外层循环用于控制从头到尾的比较+交换到底有多少轮
+  for (let i = 0; i < len; i++) {
+    // 内层循环用于完成每一轮遍历过程中的重复比较+交换
+    for (let j = 0; j < len - 1; j++) {
+      // 若相邻元素前面的数比后面的大
+      if (arr[j] > arr[j + 1]) {
+        // 交换两者
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  // 返回数组
+  return arr;
+}
+// 改进版
+function betterBubbleSort(arr) {
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    // 注意差别在这行，我们对内层循环的范围作了限制
+    for (let j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+}
+
+// console.log(betterBubbleSort([6, 3, 7, 1, 5, 9]));
+
+// 最好情况为O(n)的
+function bestBubbleSort(arr) {
+  const len = arr.length;
+
+  for (let i = 0; i < len; i++) {
+    // 区别在这里，我们加了一个标志位
+    let flag = false;
+    for (let j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        // 只要发生了一次交换，就修改标志位
+        flag = true;
+      }
+    }
+
+    // 若一次交换也没发生，则说明数组有序，直接放过
+    if (flag == false) return arr;
+  }
+  return arr;
+}
+
+// 选择排序 O(n^2)
+function selectSort(arr) {
+  // 缓存数组长度
+  const len = arr.length;
+  // 定义 minIndex，缓存当前区间最小值的索引，注意是索引
+  let minIndex;
+  // i 是当前排序区间的起点
+  for (let i = 0; i < len - 1; i++) {
+    // 初始化 minIndex 为当前区间第一个元素
+    minIndex = i;
+    // i、j分别定义当前区间的上下界，i是左边界，j是右边界
+    for (let j = i; j < len; j++) {
+      // 若 j 处的数据项比当前最小值还要小，则更新最小值索引为 j
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    // 如果 minIndex 对应元素不是目前的头部元素，则交换两者
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+  return arr;
+}
+
+// 插入排序
+// 插入排序的核心思想是“找到元素在它前面那个序列中的正确位置”。
+// 具体来说，插入排序所有的操作都基于一个这样的前提：当前元素前面的序列是有序的。基于这个前提，从后往前去寻找当前元素在前面那个序列里的正确位置。
+function insertSort(arr) {
+  // 缓存数组长度
+  const len = arr.length;
+  // temp 用来保存当前需要插入的元素
+  let temp;
+  // i用于标识每次被插入的元素的索引
+  for (let i = 1; i < len; i++) {
+    // j用于帮助 temp 寻找自己应该有的定位
+    let j = i;
+    temp = arr[i];
+    // 判断 j 前面一个元素是否比 temp 大
+    while (j > 0 && arr[j - 1] > temp) {
+      // 如果是，则将 j 前面的一个元素后移一位，为 temp 让出位置
+      arr[j] = arr[j - 1];
+      j--;
+    }
+    // 循环让位，最后得到的 j 就是 temp 的正确索引
+    arr[j] = temp;
+  }
+  return arr;
+}
+
+// 快速排序入口
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  // 定义递归边界，若数组只有一个元素，则没有排序必要
+  if (arr.length > 1) {
+    // lineIndex表示下一次划分左右子数组的索引位
+    const lineIndex = partition(arr, left, right);
+    // 如果左边子数组的长度不小于1，则递归快排这个子数组
+    if (left < lineIndex - 1) {
+      // 左子数组以 lineIndex-1 为右边界
+      quickSort(arr, left, lineIndex - 1);
+    }
+    // 如果右边子数组的长度不小于1，则递归快排这个子数组
+    if (lineIndex < right) {
+      // 右子数组以 lineIndex 为左边界
+      quickSort(arr, lineIndex, right);
+    }
+  }
+  return arr;
+}
+// 以基准值为轴心，划分左右子数组的过程
+function partition(arr, left, right) {
+  // 基准值默认取中间位置的元素
+  let pivotValue = arr[Math.floor(left + (right - left) / 2)];
+  // 初始化左右指针
+  let i = left;
+  let j = right;
+  // 当左右指针不越界时，循环执行以下逻辑
+  while (i <= j) {
+    // 左指针所指元素若小于基准值，则右移左指针
+    while (arr[i] < pivotValue) {
+      i++;
+    }
+    // 右指针所指元素大于基准值，则左移右指针
+    while (arr[j] > pivotValue) {
+      j--;
+    }
+
+    // 若i<=j，则意味着基准值左边存在较大元素或右边存在较小元素，交换两个元素确保左右两侧有序
+    if (i <= j) {
+      swap(arr, i, j);
+      i++;
+      j--;
+    }
+  }
+  // 返回左指针索引作为下一次划分左右子数组的依据
+  return i;
+}
+
+// 快速排序中使用 swap 的地方比较多，我们提取成一个独立的函数
+function swap(arr, i, j) {
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+}
